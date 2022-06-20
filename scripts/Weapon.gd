ca7e1f9752d969a23450c3deee7c97f7300a100c
Maneren <49210777+Maneren@users.noreply.github.com>
2022-06-20 18:09:07 +0200

    fix animations and add directional sprite support

--- scripts/Weapon.gd
+++ scripts/Weapon.gd
@@ -3,5 +3,9 @@ extends Node2D
 
 func _on_WeaponHitBox_body_entered(body):
 	print(self.owner.name, body.name, self.owner.name.split("_")[0], body.name.split("_")[0])
-	if self.owner.name != body.name and self.owner.name.split("_")[0] != body.name.split("_")[0]:
+	if (
+		not body.name.begins_with("StaticBody")
+		and self.owner.name != body.name
+		and self.owner.name.split("_")[0] != body.name.split("_")[0]
+	):
 		body.take_damage(self.owner.attack_damage)
--- scripts/enemies/Ghost.gd
+++ scripts/enemies/Ghost.gd
@@ -5,3 +5,7 @@ func _init():
 	xp_reward = 5
 	health = 30
 	attack_damage = 10
+
+
+func match_sprite_direction(_vec: Vector2):
+	pass
