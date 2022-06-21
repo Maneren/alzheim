0d220269e6c00e9aadbb0fc9599a6fc983da516b
Maneren <49210777+Maneren@users.noreply.github.com>
2022-06-21 21:20:08 +0200

    added moving doors to the cave

--- scripts/KinematicBody2D.gd
+++ scripts/KinematicBody2D.gd
@@ -1,6 +1,23 @@
 extends KinematicBody2D
 
+var moving = 0
 
-# move 1024 to the right
-func _ready():
-	pass # 
+
+func _on_Area2D_area_shape_entered(_area_rid, area: Area2D, _area_shape_index, _local_shape_index):
+	if area.owner.name == "Player":
+		print("Player entered area")
+		if area.owner.finished_bard_quests.size() == 4:
+			print("Opening")
+			moving = 1
+
+
+func _on_Area2D_area_shape_exited(_area_rid, area: Area2D, _area_shape_index, _local_shape_index):
+	if area.owner.name == "Player":
+		print("Player exited area")
+		print("Closing")
+		moving = -1
+
+
+func _process(delta):
+	self.position.x += moving * delta * 200
+	self.position.x = clamp(self.position.x, 0, 1024)
