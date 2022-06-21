7b39c16d15ea549a01271f81b1467b721215551b
Maneren <49210777+Maneren@users.noreply.github.com>
2022-06-21 10:34:06 +0200

    fixed weapon interactions

--- scripts/Weapon.gd
+++ scripts/Weapon.gd
@@ -3,7 +3,7 @@ extends Node2D
 
 func _on_WeaponHitBox_body_entered(body):
 	if (
-		not body.name.begins_with("StaticBody")
+		(body.name == "Player" or "_" in body.name)
 		and self.owner.name != body.name
 		and self.owner.name.split("_")[0] != body.name.split("_")[0]
 	):
