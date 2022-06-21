3ce3b62fb89a42a924739b66932365fe1995c6f6
Maneren <49210777+Maneren@users.noreply.github.com>
2022-06-22 00:26:01 +0200

    added wizard

---
+++ scripts/enemies/Wizard.gd
@@ -0,0 +1,27 @@
+extends "../Enemy.gd"
+
+
+func _process(delta):
+	attack_cooldown.tick(delta)
+
+	for node in $SearchRange.get_overlapping_bodies():
+		if node.name == "Player":
+			move_to_node(delta, node)
+			break
+
+	for node in $AttackRange.get_overlapping_bodies():
+		if node.name == "Player":
+			if first_attack:
+				first_attack = false
+				attack_cooldown.reset_with_delay(1)
+			if attack_cooldown.is_ready():
+				print("Enemy attack!")
+				attacking = true
+				$Weapon.visible = true
+
+			break
+
+	if attacking:
+		attack_tick(delta)
+	else:
+		$Weapon.visible = false
