70d9af31a9f580ad97555ea953e9ddfde02a44f8
Maneren <49210777+Maneren@users.noreply.github.com>
2022-06-21 20:58:38 +0200

    fix enemies killing themselves bug

--- scripts/Spawner.gd
+++ scripts/Spawner.gd
@@ -6,10 +6,12 @@ export(int) var enemy_spawn_interval = 1
 
 signal enemy_killed(xp_reward, name)
 
+var counter = 0
+
 
 func _ready():
 	$Timer.wait_time = enemy_spawn_interval
-	create_enemy(1)
+	create_enemy()
 
 
 func _on_Enemy_killed(xp_reward, name):
@@ -22,15 +24,18 @@ func _on_Timer_timeout():
 	if child_count >= max_enemy_count:
 		return
 
-	create_enemy(child_count)
+	create_enemy()
 
 
-func create_enemy(i):
+func create_enemy():
 	var enemy = enemy_template.instance()
 
-	var name = "Enemy_" + str(i)
+	var name = "Enemy_" + str(counter)
 	enemy.name = name
 	enemy.position = Vector2(rand_range(-50, 50), rand_range(-50, 50))
 	enemy.connect("killed", self, "_on_Enemy_killed")
 
 	add_child(enemy)
+
+	counter += 1
+	counter %= max_enemy_count
